3b22f2d669e505827d1f7b6013c723cb26916e38
Awren Nuit <awren.nuit@gmail.com>
2021-11-20 15:19:17 -0600

    refine logic; turns steps into functions; compare against tests

--- minesweeper.js
+++ minesweeper.js
@@ -1,8 +1,9 @@
 export const annotate = (input) => {
   let board = {};
   let mineCoordinates = [];
+  let output = [];
 
-  function incrementColumns(row, col) {
+  function incrementAdjacentSpaces(row, col) {
     if (board[`row${row}`]) {
       if (board[`row${row}`][`col${col - 1}`] && board[`row${row}`][`col${col - 1}`] !== '*') board[`row${row}`][`col${col - 1}`]++;
       if (board[`row${row}`][`col${col}`] && board[`row${row}`][`col${col}`] !== '*') board[`row${row}`][`col${col}`]++;
@@ -10,8 +11,28 @@ export const annotate = (input) => {
     }
   }
 
-  // set board values
-  if (input.length > 0 && input.indexOf('') === -1) {
+  function findAdjacentSpaces() {
+    for (const mine of mineCoordinates) {
+      const row = mine[0];
+      const col = mine[1];
+
+      incrementAdjacentSpaces(row - 1, col);
+      incrementAdjacentSpaces(row, col);
+      incrementAdjacentSpaces(row + 1, col);
+    }
+  }
+
+  function formatBoardForOutput() {
+    for (const row of Object.values(board)) {
+      let thisRow = '';
+      for (const col of Object.values(row)) {
+        thisRow += `${col}`;
+      }
+      output.push(thisRow);
+    }
+  }
+
+  function setBoardValues() {
     for (let i = 0; i < input.length; i++) {
       for (let j = 0; j < input[i].length; j++) {
         if (input[i][j] === '*') mineCoordinates.push([i, j]);
@@ -19,20 +40,14 @@ export const annotate = (input) => {
         board[`row${i}`][`col${j}`] = input[i][j];
       }
     }
+  }
 
-    // increment spaces adjacent to mines
-    for (const mine of mineCoordinates) {
-      const row = mine[0];
-      const col = mine[1];
+  if (input.length > 0 && input.indexOf('') === -1) {
+    setBoardValues();
+    findAdjacentSpaces();
+    formatBoardForOutput();
 
-      incrementColumns(row - 1, col);
-      incrementColumns(row, col);
-      incrementColumns(row + 1, col);
-    }
-
-    // put board back together
-
-    return board;
+    return output;
   } else {
     return input;
   }
