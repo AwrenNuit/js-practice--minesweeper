015b9d53d82333d61fa10466a1fc9603d8790d3d
Awren Nuit <awren.nuit@gmail.com>
2021-11-20 14:59:35 -0600

    create loops to set board state object; list basic steps in code comments

--- minesweeper.js
+++ minesweeper.js
@@ -1,6 +1,20 @@
 export const annotate = (input) => {
-  if (input.length > 0 && input.indexOf('') === -1) {
+  let board = {};
 
+  // set board values
+  if (input.length > 0 && input.indexOf('') === -1) {
+    for (let i = 0; i < input.length; i++) {
+      for (let j = 0; j < input.length; j++) {
+        if (!board[`row${i}`]) board[`row${i}`] = {};
+        board[`row${i}`][`col${j}`] = input[i][j];
+      }
+    }
+
+    // increment spaces adjacent to mines
+
+    // put board back together
+
+    return board;
   } else {
     return input;
   }
